2404af872fb40efac0d0d37c890baff6c2182c20
ravihara2000 <pravihara50@gmail.com>
2025-02-11 00:27:13 +0530

    Home page done

--- MyApplication/app/src/main/java/com/example/myapplication/Login.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/Login.kt
@@ -3,19 +3,17 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 
-class Login: ComponentActivity() {
+class Login : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
-
-
-        val registerLink =findViewById<TextView>(R.id.textView5)
-
-        registerLink.text=android.text.Html.fromHtml(getString(R.string.Register))
+        val registerLink = findViewById<TextView>(R.id.textView5)
+        registerLink.text = android.text.Html.fromHtml(getString(R.string.Register))
         registerLink.movementMethod = LinkMovementMethod.getInstance()
 
         // Handle clicks
@@ -24,6 +22,10 @@ class Login: ComponentActivity() {
             startActivity(intent)
         }
 
-
+        val homeLink = findViewById<Button>(R.id.loginButton)
+        homeLink.setOnClickListener {
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
     }
 }
