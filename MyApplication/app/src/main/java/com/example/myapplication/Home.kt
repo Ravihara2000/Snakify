807afc7ea68144b62949972cd471859fbd47293e
ravihara2000 <pravihara50@gmail.com>
2025-02-11 01:34:31 +0530

    About us Screen done

--- MyApplication/app/src/main/java/com/example/myapplication/Home.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/Home.kt
@@ -1,11 +1,26 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 
 class Home : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
+
+        val profileLink = findViewById<ImageButton>(R.id.imageButton2)
+        profileLink.setOnClickListener {
+            val intent = Intent(this, Profile::class.java)
+            startActivity(intent)
+        }
+
+        val aboutLink = findViewById<Button>(R.id.button2)
+        aboutLink.setOnClickListener {
+            val intent = Intent(this, About::class.java)
+            startActivity(intent)
+        }
     }
 }
